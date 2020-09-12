7083074512de4bbbdd319ee347e8ae0a31539af0
CpdnCristiano <Cristiano@proserv.net.br>
2020-09-12 02:10:56 -0300

    Add pageable

--- lib/get_server.dart
+++ lib/get_server.dart
@@ -1,3 +1,4 @@
 export 'src/context/context_request.dart';
 export 'src/core/server_main.dart';
 export 'src/routes/route.dart';
+export 'src/widgets/pageable.dart';
---
+++ lib/src/widgets/pageable.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+import 'package:get_server/get_server.dart';
+
+// this this widget to make pagination simple and easy
+class Pageable extends GetWidget {
+  List<dynamic> list;
+  int page;
+  int size;
+  Pageable(this.list, {this.page = 1, this.size = 10});
+
+  @override
+  Future build(Context context) {
+    String pageparam = context.param('page');
+    if (pageparam != null) {
+      int _page = int.parse(pageparam, onError: (_) => null);
+      if (_page == null) {
+        return context.response.status(HttpStatus.badRequest).sendJson(_Erro(
+            errocode: HttpStatus.badRequest,
+            description:
+                'The page parameter must receive an int: $pageparam it\'s not an int'));
+      }
+      page = _page;
+    }
+
+    String sizeparam = context.param('size');
+    if (sizeparam != null) {
+      int _size = int.parse(sizeparam, onError: (_) => null);
+      if (_size == null) {
+        return context.response.status(HttpStatus.badRequest).sendJson(_Erro(
+            errocode: HttpStatus.badRequest,
+            description:
+                'The size parameter must receive an int: $sizeparam it\'s not an int'));
+      }
+      size = _size;
+    }
+
+    final fistElement = (page - 1) * size;
+    final lastElement = page * size;
+
+    final totalPages = (list.length / size).ceil();
+
+    if (totalPages < page) {
+      return context.response.status(HttpStatus.badRequest).sendJson(_Erro(
+          errocode: HttpStatus.badRequest,
+          description:
+              'The reported page is larger than the maximum page: report something between 1 and $totalPages'));
+    }
+
+    final result = list.sublist(
+        fistElement, lastElement > list.length ? list.length : lastElement);
+
+    final pageable = _Pageable(
+        content: result,
+        currentPage: page,
+        size: size,
+        totalElements: list.length,
+        totalPages: totalPages);
+    return context.sendJson(pageable);
+  }
+}
+
+class _Pageable {
+  List<dynamic> content;
+  int size;
+  int totalElements;
+  int totalPages;
+  int currentPage;
+
+  _Pageable(
+      {this.content,
+      this.size,
+      this.totalElements,
+      this.totalPages,
+      this.currentPage});
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    if (content != null) {
+      if (content is List<String> ||
+          content is List<num> ||
+          content is List<bool> ||
+          content is List<DateTime>) {
+        data['content'] = content;
+      } else {
+        data['content'] = content.map((v) => v.toJson()).toList();
+      }
+    }
+    data['size'] = size;
+    data['totalElements'] = totalElements;
+    data['totalPages'] = totalPages;
+    data['currentPage'] = currentPage;
+    return data;
+  }
+}
+
+class _Erro {
+  int errocode;
+  String description;
+
+  _Erro({this.errocode, this.description});
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['errocode'] = this.errocode;
+    data['description'] = this.description;
+    return data;
+  }
+}
