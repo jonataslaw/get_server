8a9d2b07c44ca1a9336eb9b80004ac484b4af2db
Odun Adeboye <odunboye@gmail.com>
2021-03-11 12:45:09 +0100

    cors Auth fix

--- lib/src/core/src/server_main.dart
+++ lib/src/core/src/server_main.dart
@@ -206,7 +206,7 @@ class GetServer extends StatelessWidget with NodeMode {
     response.headers
         .add('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
     response.headers.add('Access-Control-Allow-Headers',
-        'access-control-allow-origin,content-type,x-access-token');
+        'access-control-allow-origin,content-type,x-access-token,authorization');
   }
 
   void _onNotFound(HttpRequest req, Widget onNotFound) {
